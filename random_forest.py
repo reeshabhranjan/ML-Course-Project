804913b212c95884196cb9f8ba07c10c3dd47383
Reeshabh <reeshabhkumarranjan@gmail.com>
2019-11-18 01:36:41 +0530

    Added code to merge aggresive and drowsy driving files as well.

--- random_forest.py
+++ random_forest.py
@@ -18,6 +18,24 @@ if __name__ == '__main__':
 			normal_timeseries[i - merging_extent] = new_row
 		normal_timeseries_list.append(normal_timeseries)
 
+	for aggressive in aggressive_list:
+		aggressive_timeseries = np.zeros((aggressive.shape[0] - merging_extent, aggressive.shape[1] * (merging_extent + 1)))
+		for i in range(merging_extent, aggressive.shape[0]):
+			new_row = aggressive[i]
+			for j in range(1, merging_extent + 1):
+				new_row = np.append(new_row, aggressive[i - j])
+			aggressive_timeseries[i - merging_extent] = new_row
+		aggressive_timeseries_list.append(aggressive_timeseries)
+
+	for drowsy in drowsy_list:
+		drowsy_timeseries = np.zeros((drowsy.shape[0] - merging_extent, drowsy.shape[1] * (merging_extent + 1)))
+		for i in range(merging_extent, drowsy.shape[0]):
+			new_row = drowsy[i]
+			for j in range(1, merging_extent + 1):
+				new_row = np.append(new_row, drowsy[i - j])
+			drowsy_timeseries[i - merging_extent] = new_row
+		drowsy_timeseries_list.append(drowsy_timeseries)
+
 
 	# normal_timeseries_list = np.zeros((normal_list[0].shape[0] - 5, 27 * 6))
 	# for i in range(5, normal_list[0].shape[0]):
