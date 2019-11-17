ddc207bc4ef6a8391ccb841cfce6fde7df70ef08
Reeshabh <reeshabhkumarranjan@gmail.com>
2019-11-18 03:23:18 +0530

    Added checks for NaN values.

--- random_forest.py
+++ random_forest.py
@@ -2,6 +2,7 @@ import data_extractor
 import numpy as np
 
 if __name__ == '__main__':
+
 	normal_list, aggressive_list, drowsy_list = data_extractor.load_all_data()
 	normal_timeseries_list = []
 	aggressive_timeseries_list = []
@@ -15,7 +16,8 @@ if __name__ == '__main__':
 			new_row = normal[i]
 			for j in range(1, merging_extent + 1):
 				new_row = np.append(new_row, normal[i - j])
-			normal_timeseries[i - merging_extent] = new_row
+			if not np.isnan(new_row).any():
+				normal_timeseries[i - merging_extent] = new_row
 		normal_timeseries_list.append(normal_timeseries)
 
 	for aggressive in aggressive_list:
@@ -24,7 +26,8 @@ if __name__ == '__main__':
 			new_row = aggressive[i]
 			for j in range(1, merging_extent + 1):
 				new_row = np.append(new_row, aggressive[i - j])
-			aggressive_timeseries[i - merging_extent] = new_row
+			if not np.isnan(new_row).any():
+				aggressive_timeseries[i - merging_extent] = new_row
 		aggressive_timeseries_list.append(aggressive_timeseries)
 
 	for drowsy in drowsy_list:
@@ -33,17 +36,15 @@ if __name__ == '__main__':
 			new_row = drowsy[i]
 			for j in range(1, merging_extent + 1):
 				new_row = np.append(new_row, drowsy[i - j])
-			drowsy_timeseries[i - merging_extent] = new_row
+			if not np.isnan(new_row).any():
+				drowsy_timeseries[i - merging_extent] = new_row
 		drowsy_timeseries_list.append(drowsy_timeseries)
 
-
-	# normal_timeseries_list = np.zeros((normal_list[0].shape[0] - 5, 27 * 6))
-	# for i in range(5, normal_list[0].shape[0]):
-	# 	new_row = normal_list[0][i]
-	# 	new_row = np.append(new_row, normal_list[0][i - 1])
-	# 	new_row = np.append(new_row, normal_list[0][i - 2])
-	# 	new_row = np.append(new_row, normal_list[0][i - 3])
-	# 	new_row = np.append(new_row, normal_list[0][i - 4])
-	# 	new_row = np.append(new_row, normal_list[0][i - 5])
-	# 	print(i)
-	# 	normal_timeseries_list[i - 5] = new_row
+	for i, normal_timeseries in enumerate(normal_timeseries_list):
+		normal_timeseries_list[i] = np.concatenate((normal_timeseries, np.full((normal_timeseries.shape[0], 1), 0)), axis=1)
+		
+	for i, aggressive_timeseries in enumerate(aggressive_timeseries_list):
+		aggressive_timeseries_list[i] = np.concatenate((aggressive_timeseries, np.full((aggressive_timeseries.shape[0], 1), 1)), axis=1)
+	
+	for i, drowsy_timeseries in enumerate(drowsy_timeseries_list):
+		drowsy_timeseries_list[i] = np.concatenate((drowsy_timeseries, np.full((drowsy_timeseries.shape[0], 1), 2)), axis=1)
