3278c47686e3ad85f5abf5023b0bd75a3cbd76ad
Reeshabh <reeshabhkumarranjan@gmail.com>
2019-11-24 19:34:31 +0530

    Started with preparation of dataset for LSTM (3-D data).

--- data_extractor.py
+++ data_extractor.py
@@ -29,8 +29,27 @@ def load_all_data():
 		elif "AGGRESSIVE" in filename:
 			_aggressive.append(np.loadtxt(filename))
 
+	# for normal in _normal:
+	# 	print(normal.shape[0])
+
 	return _normal, _aggressive, _drowsy
 
+def prepare_dataset_for_lstm():
+	normal_list, aggressive_list, drowsy_list = load_all_data()
+
+	timestamp_dimension = 100
+
+	global_array = np.zeros((0, timestamp_dimension, normal_list[0].shape[1]))
+
+	for i, normal in enumerate(normal_list):
+		for j in range(normal.shape[0] // timestamp_dimension):
+			sample = normal[j * timestamp_dimension : j * timestamp_dimension + timestamp_dimension]
+			sample = np.asarray([sample])
+			# sample = np.moveaxis(np.atleast_3d(sample), -1, 0)
+			global_array = np.append(global_array, sample, axis=0)
+
+	print(global_array.shape)
 
 if __name__ == '__main__':
-	normal, aggressive, drowsy = load_all_data()
+	# normal, aggressive, drowsy = load_all_data()
+	prepare_dataset_for_lstm()
---
+++ lstm.py
@@ -0,0 +1,2 @@
+import torch
+print(torch.cuda.get_device_name(0))
