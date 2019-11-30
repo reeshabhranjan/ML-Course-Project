e0a8ef003e38091cd4bfb84a4eed084d58548ee5
Reeshabh <reeshabhkumarranjan@gmail.com>
2019-12-01 04:27:09 +0530

    Cleaned code for submission.

--- data_extractor.py
+++ data_extractor.py
@@ -32,10 +32,6 @@ def load_all_data():
 		elif "AGGRESSIVE" in filename:
 			aggressive_list.append(np.loadtxt(filename))
 
-	# for normal in _normal:
-	# 	print(normal.shape[0])
-
-	# filtering nan values, reference: https://stackoverflow.com/a/11453235/5394180
 	for i, normal in enumerate(normal_list):
 		normal_list[i] = normal[~np.isnan(normal).any(axis=1)]
 	for i, aggressive in enumerate(aggressive_list):
@@ -58,12 +54,7 @@ def prepare_dataset_for_lstm():
 		normal = sklearn.preprocessing.normalize(normal)
 		for j in range(normal.shape[0] // timestamp_dimension):
 			sample = normal[j * timestamp_dimension : j * timestamp_dimension + timestamp_dimension]
-			# remove the time column
-			# sample = sample[:, 1:]
-			# normalise this batch
-			# sample = sklearn.preprocessing.normalize(sample)
 			sample = np.asarray([sample])
-			# sample = np.moveaxis(np.atleast_3d(sample), -1, 0)
 			global_x = np.append(global_x, sample, axis=0)
 			global_y = np.append(global_y, np.full((1, 1), 0), axis=0)
 
@@ -73,7 +64,6 @@ def prepare_dataset_for_lstm():
 		for j in range(aggressive.shape[0] // timestamp_dimension):
 			sample = aggressive[j * timestamp_dimension : j * timestamp_dimension + timestamp_dimension]
 			sample = np.asarray([sample])
-			# sample = np.moveaxis(np.atleast_3d(sample), -1, 0)
 			global_x = np.append(global_x, sample, axis=0)
 			global_y = np.append(global_y, np.full((1, 1), 1), axis=0)
 			
@@ -83,17 +73,91 @@ def prepare_dataset_for_lstm():
 		for j in range(drowsy.shape[0] // timestamp_dimension):
 			sample = drowsy[j * timestamp_dimension : j * timestamp_dimension + timestamp_dimension]
 			sample = np.asarray([sample])
-			# sample = np.moveaxis(np.atleast_3d(sample), -1, 0)
 			global_x = np.append(global_x, sample, axis=0)
 			global_y = np.append(global_y, np.full((1, 1), 2), axis=0)
 
 	return global_x, global_y
 
+def prepare_dataset_for_svm():
+	normal_list, aggressive_list, drowsy_list = load_all_data()
+	normal_timeseries_list = []  # this will contain list of datasets in form of numpy matrices
+	# that will include time-based data (by appending last 'merging-extent'
+	# rows to a row
+	aggressive_timeseries_list = []  # similarly for aggressive
+	drowsy_timeseries_list = []  # similarly for drowsy
+
+	merging_extent = 10  # This tells for ith row, how many previous rows are we appending to it to
+	# make our ML model aware of the time-based interactions
+
+	# the following six for-loops just prepares the lists defined above
+	for normal in normal_list:
+		normal_timeseries = np.zeros((normal.shape[0] - merging_extent, normal.shape[1] * (merging_extent + 1)))
+		for i in range(merging_extent, normal.shape[0]):
+			new_row = normal[i]
+			for j in range(1, merging_extent + 1):
+				new_row = np.append(new_row, normal[i - j])
+			if not np.isnan(new_row).any():
+				normal_timeseries[i - merging_extent] = new_row
+		normal_timeseries_list.append(normal_timeseries)
+
+	for aggressive in aggressive_list:
+		aggressive_timeseries = np.zeros(
+			(aggressive.shape[0] - merging_extent, aggressive.shape[1] * (merging_extent + 1)))
+		for i in range(merging_extent, aggressive.shape[0]):
+			new_row = aggressive[i]
+			for j in range(1, merging_extent + 1):
+				new_row = np.append(new_row, aggressive[i - j])
+			if not np.isnan(new_row).any():
+				aggressive_timeseries[i - merging_extent] = new_row
+		aggressive_timeseries_list.append(aggressive_timeseries)
+
+	for drowsy in drowsy_list:
+		drowsy_timeseries = np.zeros((drowsy.shape[0] - merging_extent, drowsy.shape[1] * (merging_extent + 1)))
+		for i in range(merging_extent, drowsy.shape[0]):
+			new_row = drowsy[i]
+			for j in range(1, merging_extent + 1):
+				new_row = np.append(new_row, drowsy[i - j])
+			if not np.isnan(new_row).any():
+				drowsy_timeseries[i - merging_extent] = new_row
+		drowsy_timeseries_list.append(drowsy_timeseries)
+
+	# assign labels
+	for i, normal_timeseries in enumerate(normal_timeseries_list):
+		normal_timeseries_list[i] = np.concatenate((normal_timeseries, np.full((normal_timeseries.shape[0], 1), 0)),
+		                                           axis=1)
+
+	for i, aggressive_timeseries in enumerate(aggressive_timeseries_list):
+		aggressive_timeseries_list[i] = np.concatenate(
+			(aggressive_timeseries, np.full((aggressive_timeseries.shape[0], 1), 1)), axis=1)
+
+	for i, drowsy_timeseries in enumerate(drowsy_timeseries_list):
+		drowsy_timeseries_list[i] = np.concatenate((drowsy_timeseries, np.full((drowsy_timeseries.shape[0], 1), 2)),
+		                                           axis=1)
+
+	# finally I will concatenate all the data into one dataset
+	dataset = normal_timeseries_list[0]
+
+	for i in range(1, len(normal_timeseries_list)):
+		dataset = np.concatenate((dataset, normal_timeseries_list[i]), axis=0)
+
+	for i in range(len(aggressive_timeseries_list)):
+		dataset = np.concatenate((dataset, aggressive_timeseries_list[i]), axis=0)
+
+	for i in range(len(drowsy_timeseries_list)):
+		dataset = np.concatenate((dataset, drowsy_timeseries_list[i]), axis=0)
+
+	# shuffle the dataset
+	np.random.shuffle(dataset)
+
+	# we do not need the time column anymore
+	X = dataset[:, 1:-1]
+	Y = dataset[:, -1]
+
+	# normalise the dataset
+	X = sklearn.preprocessing.normalize(X)
+
+	x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.2, random_state=2)
+	return x_train, x_test, y_train, y_test
+
 if __name__ == '__main__':
-	normal, aggressive, drowsy = load_all_data()
-	# x, y = prepare_dataset_for_lstm()
-	# x, y = sklearn.utils.shuffle(x, y, random_state=2)
-	# print(x.shape)
-	# print(y.shape)
-	# np.save("x", x)
-	# np.save("y", y)
+	pass
--- svm.py
+++ svm.py
@@ -5,86 +5,14 @@ import sklearn.preprocessing
 import sklearn.model_selection
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from sys import exit
 
 if __name__ == '__main__':
 
-	normal_list, aggressive_list, drowsy_list = data_extractor.load_all_data()
-	normal_timeseries_list = [] # this will contain list of datasets in form of numpy matrices
-								# that will include time-based data (by appending last 'merging-extent'
-								# rows to a row
-	aggressive_timeseries_list = [] # similarly for aggressive
-	drowsy_timeseries_list = [] # similarly for drowsy
-
-	merging_extent = 10 # This tells for ith row, how many previous rows are we appending to it to
-						# make our ML model aware of the time-based interactions
-
-	# the following six for-loops just prepares the lists defined above
-	for normal in normal_list:
-		normal_timeseries = np.zeros((normal.shape[0] - merging_extent, normal.shape[1] * (merging_extent + 1)))
-		for i in range(merging_extent, normal.shape[0]):
-			new_row = normal[i]
-			for j in range(1, merging_extent + 1):
-				new_row = np.append(new_row, normal[i - j])
-			if not np.isnan(new_row).any():
-				normal_timeseries[i - merging_extent] = new_row
-		normal_timeseries_list.append(normal_timeseries)
-
-	for aggressive in aggressive_list:
-		aggressive_timeseries = np.zeros((aggressive.shape[0] - merging_extent, aggressive.shape[1] * (merging_extent + 1)))
-		for i in range(merging_extent, aggressive.shape[0]):
-			new_row = aggressive[i]
-			for j in range(1, merging_extent + 1):
-				new_row = np.append(new_row, aggressive[i - j])
-			if not np.isnan(new_row).any():
-				aggressive_timeseries[i - merging_extent] = new_row
-		aggressive_timeseries_list.append(aggressive_timeseries)
-
-	for drowsy in drowsy_list:
-		drowsy_timeseries = np.zeros((drowsy.shape[0] - merging_extent, drowsy.shape[1] * (merging_extent + 1)))
-		for i in range(merging_extent, drowsy.shape[0]):
-			new_row = drowsy[i]
-			for j in range(1, merging_extent + 1):
-				new_row = np.append(new_row, drowsy[i - j])
-			if not np.isnan(new_row).any():
-				drowsy_timeseries[i - merging_extent] = new_row
-		drowsy_timeseries_list.append(drowsy_timeseries)
-
-	# assign labels
-	for i, normal_timeseries in enumerate(normal_timeseries_list):
-		normal_timeseries_list[i] = np.concatenate((normal_timeseries, np.full((normal_timeseries.shape[0], 1), 0)), axis=1)
-		
-	for i, aggressive_timeseries in enumerate(aggressive_timeseries_list):
-		aggressive_timeseries_list[i] = np.concatenate((aggressive_timeseries, np.full((aggressive_timeseries.shape[0], 1), 1)), axis=1)
-	
-	for i, drowsy_timeseries in enumerate(drowsy_timeseries_list):
-		drowsy_timeseries_list[i] = np.concatenate((drowsy_timeseries, np.full((drowsy_timeseries.shape[0], 1), 2)), axis=1)
-
-	# finally I will concatenate all the data into one dataset
-	dataset = normal_timeseries_list[0]
-
-	for i in range(1, len(normal_timeseries_list)):
-		dataset = np.concatenate((dataset, normal_timeseries_list[i]), axis=0)
-
-	for i in range(len(aggressive_timeseries_list)):
-		dataset = np.concatenate((dataset, aggressive_timeseries_list[i]), axis=0)
-
-	for i in range(len(drowsy_timeseries_list)):
-		dataset = np.concatenate((dataset, drowsy_timeseries_list[i]), axis=0)
-
-	# shuffle the dataset
-	np.random.shuffle(dataset)
-
-	# we do not need the time column anymore
-	X = dataset[:, 1:-1]
-	Y = dataset[:, -1]
-
-	# normalise the dataset
-	X = sklearn.preprocessing.normalize(X)
-
-	x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.2, random_state=2)
+	x_train, x_test, y_train, y_test = data_extractor.prepare_dataset_for_svm()
 
 	# clf = RandomForestClassifier(n_estimators=50, random_state=2)
-	clf = SVC(gamma='auto', kernel='rbf', verbose=True)
+	clf = SVC(gamma='auto', kernel='rbf', verbose=True, probability=True)
 	clf.fit(x_train, y_train)
 	print("Train accuracy: " + str(clf.score(x_train, y_train)))
 	y_pred = clf.predict(x_test)
